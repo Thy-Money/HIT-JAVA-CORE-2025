f6ccc167591e99b57a9bf4da3306da0457dae2c6
Thy-Money <truongthitiennbyk@gmail.com>
2025-07-16 21:16:57 +0700

    update gitignore

--- buoi2/BTVN/Bai1.java
+++ buoi2/BTVN/Bai1.java
@@ -1,14 +1,17 @@
+package BTVN;
+
 import java.util.Scanner;
 
 public class Bai1{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        flloat pi = 3.14;
-        do{
-            System.out.printf("nhap vao ban kinh r: ");
-            float r = sc.nextFloat();
-        }while( r>0 && r<1000);
-
-        System.out.printf(%.3(2*pi*r)+" "+%.3(pi*r*r));
-    }
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        float pi = 3.14;
+//        float r;
+//        do {
+//            System.out.print("nhap vao ban kinh r: ");
+//            r = sc.nextFloat();
+//        } while (r > 0 && r < 1000);
+//
+//        System.out.println( % .3 (2 * pi * r) + " " + % .3 (pi * r * r));
+//    }
 }
